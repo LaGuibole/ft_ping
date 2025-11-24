8d61e77c2438030c9f6d355dfafa6687204f3e49
guphilip <guphilip@LaGuiboleVM.myguest.virtualbox.org>
2025-11-24 15:52:50 +0100

    ~ | debut ajout commentaires doxygen + code pour explications

--- srcs/icmp_packets.c
+++ srcs/icmp_packets.c
@@ -6,24 +6,28 @@
 /*   By: guphilip <guphilip@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/24 12:41:19 by guphilip          #+#    #+#             */
-/*   Updated: 2025/11/24 14:38:44 by guphilip         ###   ########.fr       */
+/*   Updated: 2025/11/24 15:52:19 by guphilip         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/linker.h"
 #include "../includes/ft_ping.h"
 
-// TODO checksum, build_icmp_echo
+/// @brief Calcule le checksum 16 bits sur une zone memoire. Implementation classique ICMP (InternetChecksum)
+/// @param buffer Pointeur vers la zone memoire a sum (void *) pour de la flexinilite. La memoire est interpretee comme une sequence d'octets
+/// @param len Nombre d'octets  a prendre en compte dans le calcul
+/// @return checksum 16 bits en complement a un (valeur a passer en en-tete)
 uint16_t checksum(void *buffer, int len)
 {
-    uint32_t sum =0;
-    uint16_t *data = buffer;
+    uint32_t sum =0; // accumulateur 32 bits pour stocker les reports
+    uint16_t *data = buffer; // a traiter par mots de 16 bits quand c'est possible
     
     while (len > 1)
     {
-        sum += *data++;
-        len -= 2;
+        sum += *data++; // ajoute le mot suivant et avance le pointeur
+        len -= 2; // on a consomme 2 octets
     }
+    // s'il reste un octet, on le traite en MSB d'un mot de 16 bits
     if (len == 1)
     {
         uint16_t last = 0;
@@ -31,9 +35,10 @@ uint16_t checksum(void *buffer, int len)
         sum += last;
     }
 
+    // replier les reports de 32 bits vers 16 bits
     while (sum >> 16)
         sum = (sum & 0xFFFF) + (sum >> 16);
-    
+    // retourner le complement a un du resultat (format standard checksum internet)
     return ~sum;
 }
 
